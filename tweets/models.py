a01c75acb0bbe9a5645d1e72c8fa8508116d33ce
johnson <mobolajijohnson13@gmail.com>
2021-07-13 12:18:51 +0100

    added two more test

--- tweets/models.py
+++ tweets/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+# from django.contrib.auth.models import AbstractUser
+from django.db import models
 # Create your models here.
 
 class Tweet(models.Model):
@@ -45,3 +47,6 @@ class Comments(models.Model):
     class Meta:
         unique_together = ["user", "text"]
     
+
+
+
